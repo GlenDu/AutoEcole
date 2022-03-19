b174efc5c38ef78edad250b94c74b7c2781e046d
GlenDu <glen_llaci@yahoo.com>
2022-03-19 14:25:17 +0100

    excluding navbar and footer for landing page, accueille new page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    render layout: false
   end
+
+  def accueille
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get 'home', to: 'static_pages#home'
+  get 'accueille', to: 'static_pages#accueille'
+
   devise_for :teachers
   devise_for :students
 
