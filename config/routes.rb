49a254b07919c515976b3f9e265984db7ec60c8b
GlenDu <glen_llaci@yahoo.com>
2022-03-20 23:24:51 +0100

    Permis static page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'home', to: 'static_pages#home'
   get 'accueille', to: 'static_pages#accueille'
+  get 'permis_auto', to: 'static_pages#permis_auto'
+
 
   devise_for :teachers
   devise_for :students
