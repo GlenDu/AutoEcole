cb27a0ec955d7a55d682b4e4bb76ad2e39576b6d
GlenDu <glen_llaci@yahoo.com>
2022-04-08 09:35:48 +0200

    card of permis changed

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,13 +1,14 @@
 class StudentController < ApplicationController
   before_action :configure_sign_up_params, only: [:create]
   def index
-  end
 
-  def show
   end
 
   def new
+  end
 
+  def show
+    @current_student = Student.find_by(user_id: current_user.id)
   end
 
   def create
