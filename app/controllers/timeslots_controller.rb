e68664567e178863017e794e99a86bc1fe649af4
GlenDu <glen_llaci@yahoo.com>
2022-04-17 15:12:21 +0200

    Time slots fonctional, need refactory

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -1,6 +1,6 @@
 class TimeslotsController < ApplicationController
   def index
-    @time_slots_all = Timeslots.all
+    @time_slots_all = Timeslot.all
   end
 
   def new
@@ -9,29 +9,36 @@ class TimeslotsController < ApplicationController
 
   def create
     day_slots = Calendar.all.map {|d| d.week_day.strftime('%A')}
-    case day_slots
-    when "Monday"
-      start_slot = morning_start + (4 * 60 ** 2 )
-      end_slot = morning_start + (5 * 60 ** 2 )
-      final_slots = Timeslot.new(teacher_id: current_user.teacher.id,
-      start_slot: looping_slots(start_slot).strftime('%H:%M'),
-      end_slot: looping_slots(end_slot).strftime('%H:%M')
-      )
-      final_slots.save
-    when "Tuesday", "Wednesday", "Thursday", "Friday"
-      start_slot = morning_start
-      end_slot = morning_start + 60 ** 2
-      final_slots.save
-    when "Saturday"
-      start_slot = morning_start
-      end_slot = morning_start + 60 ** 2
-      final_slots = Timeslot.new(teacher_id: current_user.teacher.id,
-      start_slot: loop_full_day(start_slot).strftime('%H:%M'),
-      end_slot: loop_full_day(end_slot).strftime('%H:%M')
-      )
-      final_slots.save
+    day_slots.map do |day|
+      if day == "Monday"
+        length1 = looping_slots(morning_start + (4 * 3600)).length - 1
+        (0..length1).each do |x|
+          final_slots = Timeslot.new(teacher_id: current_user.teacher.id,
+          start_slot: looping_slots(morning_start + (4 * 3600 ))[x].hour.to_s,
+          end_slot: looping_slots(morning_start + (5 * 3600))[x].hour.to_s,
+          )
+          final_slots.save
+        end
+      elsif day == "Tuesday" || day == "Wednesday" || day == "Friday" || day == "Thursday"
+        length2 = loop_full_day(morning_start).length - 1
+        (0..length2).each do |x|
+          final_slots = Timeslot.new(teacher_id: current_user.teacher.id,
+          start_slot: loop_full_day(morning_start)[x].hour.to_s,
+          end_slot: loop_full_day(morning_start + 3600)[x].hour.to_s
+          )
+          final_slots.save
+        end
+      elsif day == "Saturday"
+        length2 = loop_full_day(morning_start).length - 1
+        (0..length2).each do |x|
+          final_slots = Timeslot.new(teacher_id: current_user.teacher.id,
+          start_slot: loop_full_day(morning_start)[x].hour.to_s,
+          end_slot: loop_full_day(morning_start + 3600)[x].hour.to_s
+          )
+          final_slots.save
+        end
+      end
     end
-
   end
 
   private
@@ -60,15 +67,14 @@ class TimeslotsController < ApplicationController
   end
 
   def looping_slots(slot)
-    (0..4).each do |i|
-      slot + i * 60 ** 2
+    (0..4).map do |i|
+      slot + i * 3600
     end
   end
 
   def loop_full_day(slot)
-    (0..4).each do |i|
-      start_slot + i * 60 ** 2
-      start_slot + (i + 5) * 60 ** 2
+    [0,1,2,3,4,6,7,8,9].map do |i|
+      slot + (i * 3600)
     end
   end
 
