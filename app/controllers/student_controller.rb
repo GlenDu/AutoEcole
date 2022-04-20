a009b822843156845f523c2aee772bbb50b427ab
GlenDu <glen_llaci@yahoo.com>
2022-04-20 20:05:15 +0200

    adjusting right acces to user or teacher, add profile in navbar

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,11 +1,20 @@
 class StudentController < ApplicationController
   before_action :configure_sign_up_params, only: [:create]
+  before_action :check_student, except: [:index]
+
   def index
 
   end
 
   def show
     @current_student = current_user.student
+  end
+
+  private
+  def check_student
+    unless current_user.student
+        redirect_to '/', :alert => "Don't have permission!"
+    end
   end
 
 end
