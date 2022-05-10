344a68ca3cf430bdad8d53bd4af590f0f2b64f33
GlenDu <glen_llaci@yahoo.com>
2022-05-10 23:52:23 +0200

    Creating calendar by choosing specific days

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -14,8 +14,8 @@ class CalendarController < ApplicationController
   end
 
   def create
-    (0..30).each do |i|
-      iterated_day = Date.today + i
+    (0..end_date_loop).each do |i|
+      iterated_day = start_date_selected + i #Date.today + i
       calendar = Calendar.new(:teacher_id => current_user.teacher.id,
       :reference_day => iterated_day)
       calendar.save
@@ -28,6 +28,28 @@ class CalendarController < ApplicationController
     unless current_user.teacher
         redirect_to '/', :alert => "Don't have permission!"
     end
+  end
+
+  def end_date_loop
+    (end_date_selected - start_date_selected).to_i
+  end
+
+  def start_date_selected
+    date_h1 = start_date.values.join('-')
+    return Date.strptime(date_h1, "%d-%m-%Y")
+  end
+
+  def end_date_selected
+    date_h2 = end_date.values.join('-')
+    return Date.strptime(date_h2, "%d-%m-%Y")
+  end
+
+  def start_date
+    params.require(:calendar).permit(:start_date)
+  end
+
+  def end_date
+    params.require(:calendar).permit(:end_date)
   end
 
 end
